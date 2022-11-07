ec5ec6aa2575fa48cd9ceb5b64d9fa54d723a2dc
Keegan77 <keegansb77@gmail.com>
2022-11-07 17:04:50 -0500

    Fixed up some variables, now working

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -8,7 +8,7 @@ public class ThirdPersonMovement : MonoBehaviour
     public ConstantForce force; // everything is buggy rn but just don't worry about it i'll fix it tomorrow
     private float runSpeed;
     private float speed;
-    [SerializeField] private float turnSmoothTime = 0.1f;
+    [SerializeField] private float turnSmoothTime = 0.01f;
     private Rigidbody rb;
     [SerializeField] private float jumpForce, jumpCutMultiplier;
     
@@ -32,9 +32,7 @@ public class ThirdPersonMovement : MonoBehaviour
         jump = Input.GetAxisRaw("Jump");
         sprint = Input.GetKey(KeyCode.LeftShift);
 
-
         Debug.Log(jump);
-        
     }
     private void FixedUpdate()
     {
@@ -44,31 +42,29 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized; // sets direction as hor and vert and normalizes
         float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; // gets the target angle with math n shit 
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); //makes rotation way smoother
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle - Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg, ref turnSmoothVelocity, turnSmoothTime); //easiest way to get strafing working is to just reverse the atan in the rotation damping itself
         transform.rotation = Quaternion.Euler(0f, angle, 0f); // sets rotation
 
         Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; // idk movedir makes sure it moves in the rotation of the camera's local rotation. basically turns the rotation into a direction
         if (direction.magnitude >= 0.1f) // if there is an input, do this shit
         {
-
-
             if (grounded && !sprint)
             {
                 speed = 7f;
                 rb.velocity = moveDir.normalized * speed; // moves the player. can move while grounded, air movement not implemented.
+                //rb.velocity = new Vector3(horizontal, rb.velocity.y, vertical);
             }
             if (grounded && sprint)
             {
                 runSpeed = 12f;
                 rb.velocity = moveDir.normalized * runSpeed;
             }
-
         }
         else if (grounded) // if no input, and we are grounded, this means we are sliding to a stop. this is just a scuffed way of making it deccelerate faster. WIP. not perfect by any means. i know how to do this better but im tired it's 2 am im lazy af ill fix it later
         {
             if (Mathf.Abs(rb.velocity.x) < .6f && Mathf.Abs(rb.velocity.z) < .6f) // if the abs value of the axis's are less than 1, halt the player to a stop.
             {
-                if (rb.velocity.x != 0 && rb.velocity.z != 0) // edge case for if the player is currently stopped, since we want to player to actualy move.
+                if (rb.velocity.x != 0 && rb.velocity.z != 0)
                 {
                     rb.velocity = new Vector3(0, 0, 0);
                 }
@@ -93,6 +89,9 @@ public class ThirdPersonMovement : MonoBehaviour
         {
             speed = speed * 2;
         }
+    }
+    void CalculateAirMovement()
+    {
 
     }
 
