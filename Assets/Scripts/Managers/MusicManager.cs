28c4caf841120270df36aedb5cffd0527cf9c8ce
Buvude <90856761+Buvude@users.noreply.github.com>
2022-12-02 10:02:40 -0500

    coulnd't get the dynamic feature yet, but got the fade

--- Assets/Scripts/Managers/MusicManager.cs
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    public bool doorOpened = false, franticPlaying = false;
+    private int enemiesInCloseQuarters;
     public AudioSource BasePlayer, bGMPlayer, FranticBGMPlayer;
     // Start is called before the first frame update
     void Start()
     {
         BasePlayer.volume = .3f;
-        bGMPlayer.volume = .3f;
-        FranticBGMPlayer.volume = .3f;
+        bGMPlayer.volume = .0f;
+        bGMPlayer.mute = false;
+        FranticBGMPlayer.volume = .0f;
+        FranticBGMPlayer.mute = false;
     }
 
     // Update is called once per frame
@@ -31,22 +35,82 @@ public class MusicManager : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer.ToString() == "Enemy")
+        {
+            if (enemiesInCloseQuarters == 0 &&!doorOpened)
+            {
+                CombatTrack();
+            }
+            enemiesInCloseQuarters++;
+            Debug.Log("+"+enemiesInCloseQuarters);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer.ToString() == "Enemy")
+        {
+            if (enemiesInCloseQuarters == 0 &&!doorOpened)
+            {
+                backToNormal();
+            }
+        }
+    }
+
     //hard switch for music TODO: possibly add a fading switch
     public void backToNormal()
     {
-        bGMPlayer.mute = true;
-        FranticBGMPlayer.mute = true;
+        /*bGMPlayer.mute = true;
+        FranticBGMPlayer.mute = true;*/
+        StartCoroutine("BackToNormalFade");
     }
 
     public void CombatTrack()
     {
-        bGMPlayer.mute = false;
-        FranticBGMPlayer.mute = true;
+        /* bGMPlayer.mute = false;
+         FranticBGMPlayer.mute = true;*/
+        StartCoroutine("CombatTrackFadeIn");
     }
 
     public void FranticTrack()
     {
-        bGMPlayer.mute = false;
-        FranticBGMPlayer.mute = false;
+        /* bGMPlayer.mute = false;
+         FranticBGMPlayer.mute = false;*/
+        StartCoroutine("FranticTrackFadeIn");
+    }
+    IEnumerator CombatTrackFadeIn()
+    {
+        while (bGMPlayer.volume < .30f)
+        {
+            bGMPlayer.volume += .01f;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator BackToNormalFade()
+    {
+       
+        while (bGMPlayer.volume > .00f)
+        {
+            bGMPlayer.volume -= .01f;
+            yield return new WaitForEndOfFrame();
+        }
+       
+    }
+
+    IEnumerator FranticTrackFadeIn()
+    {
+        while (bGMPlayer.volume < .30f)
+        {
+            bGMPlayer.volume += .01f;
+            yield return new WaitForEndOfFrame();
+        }
+        while (FranticBGMPlayer.volume < .30f)
+        {
+            bGMPlayer.volume += .01f;
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
