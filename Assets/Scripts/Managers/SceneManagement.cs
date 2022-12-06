91f4973bbeb958deb1f17fa6320dfb5352c9746c
Buvude <90856761+Buvude@users.noreply.github.com>
2022-12-06 11:21:14 -0500

    working on scene manger

--- Assets/Scripts/Managers/SceneManagement.cs
+++ Assets/Scripts/Managers/SceneManagement.cs
@@ -13,7 +13,7 @@ public class SceneManagement : MonoBehaviour
     {
         curScene = SceneManager.GetActiveScene();
         string sceneName = curScene.name;
-        if (curScene.buildIndex == 0)
+        if (curScene.buildIndex == 1)
         {
             stats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
         }
@@ -22,18 +22,22 @@ public class SceneManagement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (curScene.buildIndex == 1)
+        if (curScene.buildIndex == 2)
         {
             if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.T))
             {
                 SceneManager.LoadScene(0);
             }
         }
-        if (curScene.buildIndex == 0)
+        if (curScene.buildIndex == 1)
         {
             if (stats.CurHealth <= 0)
             {
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(2);
             }
         }
     }
