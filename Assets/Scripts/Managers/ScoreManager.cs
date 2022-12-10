d5b4328b6fea27fea3ded0460b6fb4b1dbdfca27
Buvude <90856761+Buvude@users.noreply.github.com>
2022-12-10 11:31:29 -0500

    score system is janky... but mostly working

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    public int jellyFishKilled, hazmatDudesKilled, //tracks how many of each were killed
+        jellyFishWorth, hazmatDudesWorth, //tracks the point value for each kill
+        totalScore;//total score
+    public bool vicotry = false;
+    public enum EnemyType { JellyFish, HazmatDude}
     // Start is called before the first frame update
     
     void Start()
@@ -15,5 +20,21 @@ public class ScoreManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Kill(EnemyType whoDied)
+    {
+        switch (whoDied)
+        {
+            case EnemyType.JellyFish:
+                jellyFishKilled++;
+                totalScore += jellyFishWorth;
+                break;
+            case EnemyType.HazmatDude:
+                hazmatDudesKilled++;
+                totalScore += hazmatDudesWorth;
+                break;
+            default:
+                break;
+        }
     }
 }
