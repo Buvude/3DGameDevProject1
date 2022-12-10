8054d60dc17067fd7d88d057c842ea6ef703a641
Buvude <90856761+Buvude@users.noreply.github.com>
2022-12-10 11:12:04 -0500

    working on scoring system

---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    public static Singleton Instance { get; private set; }
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
